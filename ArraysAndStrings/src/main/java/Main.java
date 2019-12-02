33455da490e6a93575045badf96cd68b7661b162
axroto <artemiirotovskii@gmail.com>
2019-12-02 23:12:54 +0500

    Arrays and Strings 1.7. rotateMatrix

--- ArraysAndStrings/src/main/java/Main.java
+++ ArraysAndStrings/src/main/java/Main.java
@@ -32,6 +32,16 @@ public class Main {
         System.out.println("compress(abcabbbbbbb): " + compress("abcabbbbbbb"));
 
 
+        System.out.println("----- rotateMatrix -----");
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        System.out.println("rotateMatrix(int[][] matrix)");
+        rotateMatrix(matrix);
+
 
     }
 
@@ -231,6 +241,45 @@ public class Main {
         }
 
         return s.length() > compressed.length() ? compressed.toString() : s;
+    }
+
+
+    /*
+    * Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
+                bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+    */
+    private static void rotateMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix.length != matrix[0].length) {
+            throw new IllegalArgumentException();
+        }
+
+        int n = matrix.length;
+
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int tempTop = matrix[first][i];
+
+                // left -> top
+                matrix[first][i] = matrix[last - offset][first];
+                // bottom -> left
+                matrix[last - offset][first] = matrix[last][last - offset];
+                // right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+                // top -> right
+                matrix[i][last] = tempTop;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + ", ");
+            }
+        }
     }
 
 
