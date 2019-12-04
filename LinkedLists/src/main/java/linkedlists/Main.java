29d12854452823ddcfbeed2d619e340043c1870c
axroto <artemiirotovskii@gmail.com>
2019-12-04 14:15:14 +0500

    LinkedLists 2.2. get Kth from end

--- LinkedLists/src/main/java/linkedlists/Main.java
+++ LinkedLists/src/main/java/linkedlists/Main.java
@@ -10,8 +10,16 @@ public class Main {
         removeDuplicatesWithBuffer(list);
         System.out.println(list);
 
+        System.out.println("----- getKthToLast -----");
+        list = new LinkedList<>(Arrays.asList(1, 1, 2, 2, 3, 4, 5, 6, 6, 6, 6));
+        System.out.println(getKthToLast(list, 10));
+
+
     }
 
+    /*
+    * Remove Dups! Write code to remove duplicates from an unsorted linked list.
+    * */
     private static void removeDuplicatesWithBuffer(LinkedList<Integer> linkedList) {
         Iterator<Integer> it = linkedList.iterator();
         HashSet<Integer> values = new HashSet<>();
@@ -23,6 +31,25 @@ public class Main {
                 values.add(current);
             }
         }
+    }
+
+    /*
+    * Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
+    * */
+    private static Integer getKthToLast(LinkedList<Integer> linkedList, int k) {
+        Iterator<Integer> p1 = linkedList.iterator();
+        Iterator<Integer> p2 = linkedList.iterator();
+
+        for (int i = 0; i < k; i++) {
+            p2.next();
+        }
+
+        while (p2.hasNext()) {
+            p2.next();
+            p1.next();
+        }
+
+        return p1.next();
     }
 
 }
