a9419ad974bd703c95527a47405849301828a849
axroto <artemiirotovskii@gmail.com>
2019-12-04 21:14:13 +0500

    LinkedLists 2.5. sum lists

--- LinkedLists/src/main/java/linkedlists/Main.java
+++ LinkedLists/src/main/java/linkedlists/Main.java
@@ -14,6 +14,11 @@ public class Main {
         list = new LinkedList<>(Arrays.asList(1, 1, 2, 2, 3, 4, 5, 6, 6, 6, 6));
         System.out.println(getKthToLast(list, 10));
 
+        System.out.println("----- addLists -----");
+        list = new LinkedList<>(Arrays.asList(1, 9, 9));
+        LinkedList<Integer> list2 = new LinkedList<>(Arrays.asList(0, 9, 9));
+        System.out.println(addLists(list, list2));
+
 
     }
 
@@ -39,7 +44,6 @@ public class Main {
     private static Integer getKthToLast(LinkedList<Integer> linkedList, int k) {
         Iterator<Integer> p1 = linkedList.iterator();
         Iterator<Integer> p2 = linkedList.iterator();
-
         for (int i = 0; i < k; i++) {
             p2.next();
         }
@@ -50,6 +54,28 @@ public class Main {
         }
 
         return p1.next();
+    }
+
+    /*
+    * Sum Lists: You have two numbers represented by a linked list, where each node contains a single
+                digit. The digits are stored in reverse order, such that the 1 's digit is at the head of the list. Write a
+                function that adds the two numbers and returns the sum as a linked list.
+    * */
+    private static LinkedList<Integer> addLists(LinkedList<Integer> n1, LinkedList<Integer> n2) {
+        LinkedList<Integer> result = new LinkedList<>();
+        carry(n1.iterator(), n2.iterator(), result);
+        return result;
+    }
+
+    private static int carry(Iterator<Integer> n1, Iterator<Integer> n2, LinkedList<Integer> result) {
+        if (!n1.hasNext()) return 0;
+        Integer val1 = n1.next();
+        Integer val2 = n2.next();
+
+        int sum = val1 + val2 + carry(n1, n2, result);
+        int digitToWrite = sum % 10;
+        result.push(digitToWrite);
+        return sum > 10 ? 1: 0;
     }
 
 }
